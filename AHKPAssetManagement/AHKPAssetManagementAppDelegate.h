152aa5814f5a7e4d679e7d0a0903950d53bef7dc
kevin henderson perez <kpere061@tatooine.cs.fiu.edu>
2011-12-04 20:14:52 -0500

    Initial Commit

---
+++ AHKPAssetManagement/AHKPAssetManagementAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AHKPAssetManagementAppDelegate.h
+//  AHKPAssetManagement
+//
+//  Created by kevin henderson perez on 12/4/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AHKPAssetManagementAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
---
+++ AHKPAssetManagement/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  AHKPAssetManagement
+//
+//  Created by kevin henderson perez on 12/4/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
